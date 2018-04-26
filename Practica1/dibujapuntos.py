c4a96bf3ad9e8253d3f6e88baf45c0062c46c842
Eva Gala <vuvuxka@gmail.com>
2018-04-26 09:18:09 +0200

    -
    -

--- Practica1/dibujapuntos.py
+++ Practica1/dibujapuntos.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Circle
+from matplotlib.lines import Line2D
 from parte3 import *
 
 
@@ -19,8 +20,8 @@ class CreatePoints(object):
     
     def __init__(self, fig, ax):
         self.circle_list = []
-        self.clase_1 = []
-        self.clase_2 = []
+        self.clase_1 = 0
+        self.clase_2 = 0
 
         self.x0 = None
         self.y0 = None
@@ -37,6 +38,7 @@ class CreatePoints(object):
 
         self.press_event = None
         self.current_circle = None
+        self.line = None
 
     def on_press(self, event):
 #        if event.button == 3:
@@ -60,11 +62,17 @@ class CreatePoints(object):
         if event.button == 3:
             c = Circle((x0, y0), 0.5, fc="r")
             self.circle_list.append(c)
-            self.clase_1.append(np.array([[x0],[y0]]))
+            if isinstance(self.clase_2, np.ndarray):
+                self.clase_2 = np.c_[self.clase_2,np.array([x0,y0])]
+            else:
+                self.clase_2 = np.array([[x0],[y0]])
         else:
             c = Circle((x0, y0), 0.5, fc="b")
             self.circle_list.append(c)
-            np.concatenate((p,p.T), axis=1 )
+            if isinstance(self.clase_1, np.ndarray):
+                self.clase_1 = np.c_[self.clase_1,np.array([x0,y0])]
+            else:
+                self.clase_1 = np.array([[x0],[y0]])
         self.ax.add_patch(c)
         self.current_circle = None
         self.fig.canvas.draw()
@@ -72,14 +80,36 @@ class CreatePoints(object):
     def on_release(self, event):
         self.press_event = None
         self.current_circle = None
-        t1 = len(self.clase_1)
-        t2 = len(self.clase_2)
-        t = np.array([np.concatenate(((np.ones(t1))[:], np.zeros(t2)), axis=0),
-                      np.concatenate(((np.zeros(t1))[:], np.ones(t2)), axis=0)])
-        #x = np.concatenate((self.circle_list_1[:], self.circle_list_2[:]), axis=0)
-        print (t)
-        print(self.clase_1)
-       # lda()
+        t1 = 0
+        t2 = 0
+        if isinstance(self.clase_1, np.ndarray):
+            t1 = len(self.clase_1[1,:])
+        if isinstance(self.clase_2, np.ndarray):
+            t2 = len(self.clase_2[1,:])
+        t = np.c_[np.r_[np.ones(t1), np.zeros(t2)], np.r_[np.zeros(t1), np.ones(t2)]].T
+        if not isinstance(self.clase_1, np.ndarray):
+            X = self.clase_2
+        elif not isinstance(self.clase_2, np.ndarray):
+            X = self.clase_1
+        else:
+            X = np.concatenate((self.clase_1, self.clase_2), axis=1)
+        w = lda(X.T,t)
+        if not isinstance(w,np.ndarray):
+            return
+        a = w[0]
+        b = w[1]
+        ## Plot functions and a point where they intersect
+        l =  Line2D((-20,20),((-20)*a+b,(20)*a+b))
+        if self.line == None:
+            self.line = l
+            self.ax.add_line(self.line)
+            print("en no")
+        else: 
+            print("en si")
+            self.ax.get_lines()[0].set_data((-20,20),((-20)*a+b,(20)*a+b))
+        self.fig.canvas.draw()
+        
+        
 
     def on_move(self, event):
         if (self.press_event is None or
@@ -91,3 +121,16 @@ class CreatePoints(object):
         dy = event.ydata - self.press_event.ydata
         self.current_circle.center = self.x0 + dx, self.y0 + dy
         self.fig.canvas.draw()
+
+
+
+#QUITAR
+fig = plt.figure()
+ax = plt.subplot(111)
+ax.set_xlim(-20, 20)
+ax.set_ylim(-20, 20)
+ax.set_aspect('equal')  
+
+start = CreatePoints(fig, ax)
+
+plt.show()
--- Practica1/parte3.py
+++ Practica1/parte3.py
@@ -13,26 +13,47 @@ def lda(X, t)
 """
 import numpy as np
 from numpy.linalg import inv
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 def lda(X, t):
-    X1 = X[(t==0),:]; "Consideramos las clases como 0 y mayor que 0"
-    X2 = X[(t>0),:];
+    X1 = X[(t[0,:]==1),:];
+    X2 = X[(t[1,:]==1),:];
     "Calculamos las medias"
     m1 = np.mean(X1, axis=0);
     m2 = np.mean(X2, axis=0);
-    n = len(X[1,:]);
+    n = len(X[0,:]);
     Sw1 = np.zeros((n, n));
     Sw2 = np.zeros((n, n));
+    
     for x_1 in X1:
         Sw1 = np.add(np.dot((x_1-m1)[:,np.newaxis],((x_1-m1)[:,np.newaxis]).T), Sw1);
+    
     for x_2 in X2:
         Sw2 = np.add(np.dot((x_2-m2)[:,np.newaxis],((x_2-m2)[:,np.newaxis]).T), Sw2);
+    
     Sw = np.add(Sw1,Sw2);
+    
+    Sw = np.array ([[0.2827, 22.9167],[22.9167, 47098.1844]])
+    
     Sb = np.dot((m2-m1)[:,np.newaxis],((m2-m1)[:,np.newaxis]).T);
-    A = np.dot(np.linalg.inv(np.matrix(Sw)), Sb);
+    if np.linalg.det(Sw) == 0:
+        return False
+    A = np.dot(np.linalg.inv(Sw), Sb);
     tmp = np.linalg.eig(A);
     eigenval, eigv = tmp;
     tmp = np.argmin(eigenval);
-    separador = np.delete(eigv, (tmp), axis=0);
+    separador = np.delete(eigv, (tmp), axis=1)[:,0];
+
+    
     return separador;
-t = lda(np.array([[7,4],[5,8],[3,5], [6,1], [5,2]]), np.array([1,0,0,1,1]))
+
+
+
+X = np.array([[14.23,1065],[12.37,520],[13.20,1050],[12.33,680],[13.16,1185],[12.64,450],[14.37,1480],[13.67,630],[13.24,735],[12.37,420],[14.20,1450],[12.17,355],[14.39,1290],[12.37,678],[14.06,1295],[13.11,502],[14.83,1045],[12.37,510],[13.86,1045],[13.34,750],[14.10,1510],[12.21,718],[14.12,1280],[12.29,870],[13.75,1320],[13.86,410],[14.75,1150],[13.49,472],[14.38,1547],[12.99,985],[13.63,1310],[14.30,1280],[13.83,1130],[14.19,1680],[13.64,845]]
+)
+t = lda(X, np.array([[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1],[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0]]
+))
+clf = LinearDiscriminantAnalysis()
+clf.fit(X, y)
+l = clf.coef_
+l2 = clf.intercept_
